0f8596c6cb2fa6757f06b10d2b129261e1832f90
Eddison Chen <eddisonchen1@gmail.com>
2022-09-21 15:17:00 -0400

    added a never switching sim

--- game/game2.py
+++ game/game2.py
@@ -2,17 +2,22 @@ import random
 
 wins = []
 losses = []
+stay_wins = []
+stay_losses = []
 
 def run_trial(win_door, selected_door):
     if selected_door == win_door:
         wins.append(1)
     elif selected_door != win_door:
         losses.append(1)
-    return wins, losses
 
 def print_results():
     win_percentage = sum(list(wins))/(sum(list(wins)) + sum(list(losses)))
-    print(f"Always switching, your win percentage is {win_percentage}%")
+    print(f"Always switching, your win percentage is {win_percentage * 100}%")
+
+def print_stay_results():
+    win_percentage = sum(list(stay_wins))/(sum(list(stay_wins)) + sum(list(stay_losses)))
+    print(f"Never switching, your win percentage is {win_percentage * 100}%")
 
 def switch_doors():
     i = 0
@@ -43,4 +48,21 @@ def switch_doors():
         i += 1
     print_results()
 
-switch_doors()
+def stay_door():
+    i = 0
+    while i < 1000000:
+        selected_door = random.choice([1, 2, 3])
+        win_door = random.choice([1, 2, 3])
+        if win_door == selected_door:
+            stay_wins.append(1)
+        elif win_door != selected_door:
+            stay_losses.append(1)
+        run_trial(win_door, selected_door)
+        i += 1
+    print_stay_results()
+
+def both_sims():
+    switch_doors()
+    stay_door()
+
+both_sims()
