70f001e2c019308338620fd64f4e21e750ac335f
Eddison Chen <eddisonchen1@gmail.com>
2022-09-21 14:31:50 -0400

    first commit

---
+++ game/game.py
@@ -0,0 +1,48 @@
+import random
+
+def run_trial(switch_doors, ndoors=3):
+    """
+    Run a single trial of the Monty Hall problem, with or without switching
+    after the gameshow host reveals a goat behind one of the unchosen doors.
+    (switch_doors is True or False). The car is behind door number 1 and the
+    gameshow host knows that.
+
+    """
+
+    # Pick a random door out of the ndoors available
+    chosen_door = random.randint(1, ndoors)
+    if switch_doors:
+        # Reveal a goat
+        revealed_door = 3 if chosen_door==2 else 2
+        # Make the switch by choosing any other door than the initially-
+        # selected one and the one just opened to reveal a goat. 
+        available_doors = [dnum for dnum in range(1,ndoors+1)
+                                if dnum not in (chosen_door, revealed_door)]
+        chosen_door = random.choice(available_doors)
+
+    # You win if you picked door number 1
+    return chosen_door == 1
+
+def run_trials(ntrials, switch_doors, ndoors=3):
+    """
+    Run ntrials iterations of the Monty Hall problem with ndoors doors, with
+    and without switching (switch_doors = True or False). Returns the number
+    of trials which resulted in winning the car by picking door number 1.
+
+    """
+
+    nwins = 0
+    for i in range(ntrials):
+        if run_trial(switch_doors, ndoors):
+            nwins += 1
+    return nwins
+
+ndoors, ntrials = 3, 10000
+nwins_without_switch = run_trials(ntrials, False, ndoors)
+nwins_with_switch = run_trials(ntrials, True, ndoors)
+
+print('Monty Hall Problem with {} doors'.format(ndoors))
+print('Proportion of wins without switching: {:.4f}'
+            .format(nwins_without_switch/ntrials))
+print('Proportion of wins with switching: {:.4f}'
+            .format(nwins_with_switch/ntrials))
---
+++ game/game2.py
@@ -0,0 +1,46 @@
+import random
+
+wins = []
+losses = []
+
+def run_trial(win_door, selected_door):
+    if selected_door == win_door:
+        wins.append(1)
+    elif selected_door != win_door:
+        losses.append(1)
+    return wins, losses
+
+def print_results():
+    win_percentage = sum(list(wins))/(sum(list(wins)) + sum(list(losses)))
+    print(win_percentage)
+
+def switch_doors():
+    i = 0
+    while i < 1000000:
+        selected_door = random.choice([1, 2, 3])
+        win_door = random.choice([1, 2, 3])
+        if win_door == selected_door:
+            if win_door == 1 and selected_door == 1:
+                selected_door = random.randint(2, 3)
+            elif win_door == 2 and selected_door == 2:
+                selected_door = random.randint(1, 3)
+            elif win_door == 3 and selected_door == 3:
+                selected_door = random.randint(1, 2)
+        elif win_door != selected_door:
+            if win_door == 1 and selected_door == 2:
+                selected_door = 1
+            elif win_door == 1 and selected_door == 3:
+                selected_door = 1
+            elif win_door == 2 and selected_door == 3:
+                selected_door = 2
+            elif win_door == 2 and selected_door == 1:
+                selected_door = 2
+            elif win_door == 3 and selected_door == 1:
+                selected_door = 3
+            elif win_door == 3 and selected_door == 2:
+                selected_door = 3
+        run_trial(win_door, selected_door)
+        i += 1
+    print_results()
+
+switch_doors()
